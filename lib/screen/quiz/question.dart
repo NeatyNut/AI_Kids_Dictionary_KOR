119acedd68957ae31afc204d59f444afd718ee08
최강훈 <coding_rkdgns94@naver.com>
2024-04-06 22:22:21 +0900

    2024-04-06 update
    quiz question update code

--- lib/screen/quiz/question.dart
+++ lib/screen/quiz/question.dart
@@ -23,88 +23,112 @@ class Question_Screen extends StatefulWidget {
 
 class _Question_ScreenState extends State<Question_Screen> {
   late final audioplay _audioPlayer;
+  final List<Color> color_list = [
+    CustomColor().green(),
+    CustomColor().blue(),
+    CustomColor().red()
+  ];
+  late Future<void> _ttsFuture;
 
   @override
   void initState() {
     super.initState();
     _audioPlayer = audioplay();
+    _ttsFuture =
+        _playQuestionAudio(widget.questionIndex); // TTS 함수 호출 및 Future 객체 생성
   }
 
   @override
   Widget build(BuildContext context) {
-    // 질문 음성을 재생합니다.
-    _playQuestionAudio(widget.questionIndex);
-
-    return Column(
-      children: [
-        TitleBanner(text: '퀴즈! 퀴즈!'),
-        SizedBox(height: 30),
-        if (widget.questionList.isNotEmpty && widget.questionIndex < widget.questionList.length)
-          Padding(
-            padding: EdgeInsets.all(20),
-            child: Column(
-              children: [
-                SingleChildScrollView(
-                  child: Container(
-                    width: double.infinity,
-                    color: Colors.transparent,
-                    child: Column(
-                      children: [
-                        Text(widget.questionList[widget.questionIndex]['question']),
-                      ],
-                    ),
+    return FutureBuilder<void>(
+      future: _ttsFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          // TTS 함수가 아직 완료되지 않은 경우, 로딩 인디케이터 표시
+          return Center(
+            child: Image.asset('assets/images/Quiz_loading.gif'),
+          );
+        } else {
+          // TTS 함수가 완료된 후에 위젯을 빌드
+          return Column(
+            children: [
+              TitleBanner(text: '퀴즈! 퀴즈!'),
+              SizedBox(height: 30),
+              if (widget.questionList.isNotEmpty &&
+                  widget.questionIndex < widget.questionList.length)
+                Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      SingleChildScrollView(
+                        child: Container(
+                          width: double.infinity,
+                          color: Colors.transparent,
+                          child: Column(
+                            children: [
+                              Text(widget.questionList[widget.questionIndex]
+                                  ['question']),
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 30),
+                      ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: widget
+                            .questionList[widget.questionIndex]['answer']
+                            .length,
+                        itemBuilder: (context, index) {
+                          return Column(
+                            children: [
+                              ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: color_list[index],
+                                  foregroundColor: Colors.white,
+                                  fixedSize: Size(300, 60),
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(20)),
+                                ),
+                                onPressed: () => widget.answerPressed(
+                                    widget.questionList[widget.questionIndex]
+                                        ['answer'][index]['score']),
+                                child: Text(
+                                    widget.questionList[widget.questionIndex]
+                                        ['answer'][index]['text']),
+                              ),
+                              SizedBox(
+                                height: 20,
+                              )
+                            ],
+                          );
+                        },
+                      ),
+                    ],
                   ),
                 ),
-                SizedBox(height: 30),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: CustomColor().green(),
-                    foregroundColor: Colors.white,
-                    fixedSize: Size(300, 60),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20)),
-                  ),
-                  onPressed: () => widget.answerPressed(widget.questionList[widget.questionIndex]['answer'][0]['score']),
-                  child: Text(widget.questionList[widget.questionIndex]['answer'][0]['text']),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: CustomColor().blue(),
-                    foregroundColor: Colors.white,
-                    fixedSize: Size(300, 60),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20)),
-                  ),
-                  onPressed: () => widget.answerPressed(widget.questionList[widget.questionIndex]['answer'][1]['score']),
-                  child: Text(widget.questionList[widget.questionIndex]['answer'][1]['text']),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: CustomColor().red(),
-                    foregroundColor: Colors.white,
-                    fixedSize: Size(300, 60),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20)),
-                  ),
-                  onPressed: () => widget.answerPressed(widget.questionList[widget.questionIndex]['answer'][2]['score']),
-                  child: Text(widget.questionList[widget.questionIndex]['answer'][2]['text']),
-                ),
-              ],
-            ),
-          ),
-      ],
+            ],
+          );
+        }
+      },
     );
   }
 
   // 질문 음성을 재생하는 메서드
   Future<void> _playQuestionAudio(int index) async {
-    Uint8List? voice = await GetSound(text: widget.questionList[index]['question']).get_voice();
+    Uint8List? voice =
+        await GetSound(text: widget.questionList[index]['question'])
+            .get_voice();
     _audioPlayer.byteplay(voice!);
+  }
+
+  @override
+  void didUpdateWidget(Question_Screen oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.questionIndex != widget.questionIndex) {
+      // 질문이 변경되었을 때만 TTS 함수 호출 및 Future 객체 업데이트
+      setState(() {
+        _ttsFuture = _playQuestionAudio(widget.questionIndex);
+      });
+    }
   }
 }
